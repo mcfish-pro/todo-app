657ecf3a236f52ec742b5663c7de658026d9fd4a
mcfish-pro <mcfish.pro@gmail.com>
2024-02-29 04:15:25 +0800

    CSS 介面

--- src/App.vue
+++ src/App.vue
@@ -47,12 +47,151 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica, "Microsoft JhengHei";
+}
+
+/* 整個頁面 */
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background: rgb(203, 210, 240);
+}
+
+.container {
+  width: 60%;
+  min-width: 400px;
+  box-shadow: 0px 0px 24px rgb(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background-color: rgb(245, 246, 252);
+}
+
+h1 {
+  margin: 24px 0;
+  font-size: 28px;
+  color: #414873;
+}
+
+/* 添加框 */
+.input-add {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.input-add input {
+  padding: 16px 52px 16px 18px;
+  border-radius: 48px;
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.08);
+  width: 100%;
+  font-size: 16px;
+  color: #626262;
+}
+
+.input-add button {
+  width: 46px;
+  height: 46px;
+  border-radius: 50%;
+  /* 以上三個組合能將 + 變成圓型 */
+
+  background: linear-gradient(#C0A5F3, #7F95F7);
+  border: none; /* 去邊線 */
+  outline: none; /* 去外邊線 */
+  color: white;
+  position: absolute; /* 使用絕對定位 */
+  right: 0px; /* 放置 input 最右邊 */
+  cursor: pointer; /* 鼠標設置為小手 */
+}
+
+input-add .plus {
+  display: block;
+  width: 100%;
+  height: 100%;
+  /* 完成直線與橫線，成為一個加號 */
+  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff);
+  background-size: 50% 2px, 2px 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.filters {
+  display: flex;
+  margin: 24px 2px;
+  color: #C0C2CE; /* 默認淺灰色，未選中狀態 */
+  font-size: 14px;
+}
+
+.filters .filter {
+  margin-right: 14px;
+  transition: 0.8s;
+}
+
+/* 選中狀態 */
+.filters .filter.active {
+  color: #6B729C;
+  transform: scale(1.2);
+}
+
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+
+.todo-item {
+  background: white;
+  padding: 16px;
+  border-radius: 8px;
+  color: #626262;
+}
+
+.todo-item label {
+  position: relative; /* 相對定位 */
+  display: flex;
+  align-items: center;
+}
+
+.todo-item label span.check-button {
+  position: absolute;
+  top: 0;
+}
+
+/* 控制 checkbox 為圓型 */
+.todo-item label span.check-button::before,
+.todo-item label span.check-button::after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+}
+
+.todo-item label span.check-button::before {
+  border: 1px solid #B382F9;
+}
+
+.todo-item label span.check-button::after {
+  transition: 0.4s;
+  background: #B382F9;
+  transform: translate(1px, 1px) scale(0.8);
+  opacity: 0;
+}
+
+.todo-item input {
+  margin-right: 16px;
+  opacity: 0;
+}
+
+.todo-item input:checked + span.check-button::after {
+  opacity: 1;
 }
 </style>
